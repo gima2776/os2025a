8f53b02f077178ced9cf892ee70e9108a66796af
gima2776 <gima2776@gmail.com>
2025-09-29 12:13:37 +0900

    6.2

--- 6/types/main.go
+++ 6/types/main.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	// ver name string
@@ -13,8 +11,26 @@ func main() {
 	// name := "kim anna"
 	// fmt.Println(name)
 
-	ver name float32
-	name := 2.71
-	// name = 2.71
-	fmt.Println(name, reflect.TypeOF(name))
+	// ver name float64
+	// // name := 2.71
+	// // name = 2.71
+	// fmt.Println(name, reflect.TypeOF(name))
+
+	// var name string
+	// var name int32
+	// var name bool
+
+	// var f64 float64
+	// var str string
+	// var i32 int32
+	// var b bool
+
+	// ver 64f float64
+	// fmt.Println(64f, reflect.TypeOf(64f))
+
+	totalPrice := 500
+	fmt.Println(totalprice)
+
+	// totalPrice := 1000
+	// fmt.Println(totalprice)
 }
