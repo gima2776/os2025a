1c960c9ab6823076694ec854dc0f1ea5c8c770d3
gima2776 <gima2776@gmail.com>
2025-10-13 11:00:42 +0900

    loops/main.go v0.1 bufio & os package

--- 7/loops/main.go
+++ 7/loops/main.go
@@ -14,20 +14,34 @@
 // 	fmt.Println("원본", reflect.TypeOf(int(length)))
 // }
 
+// package main
+
+// import (
+// 	"fmt"
+// 	"strings"
+// 	"time"
+// )
+
+// func main() {
+// 	var now time.Time = time.Now()
+// 	var day int = now.Day()
+// 	fmt.Println(day)
+// 	univ := "Go$ Inha$"
+// 	changer := strings.NewReplacer("$", "!")
+// 	changed := changer.Replace(univ)
+// 	fmt.Println(changed)
+// }
+
 package main
 
 import (
+	"bufio"
 	"fmt"
-	"strings"
-	"time"
+	"os"
 )
 
 func main() {
-	var now time.Time = time.Now()
-	var day int = now.Day()
-	fmt.Println(day)
-	univ := "Go$ Inha$"
-	changer := strings.NewReplacer("$", "!")
-	changed := changer.Replace(univ)
-	fmt.Println(changed)
+	r := bufio.NewReader(os.Stdin)
+	i, _ := r.ReadString('\n')
+	fmt.Println(i)
 }
