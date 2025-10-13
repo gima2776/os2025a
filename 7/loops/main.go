3ea0865da9b76129086e3abeead5e57aac9d1202
gima2776 <gima2776@gmail.com>
2025-10-13 10:50:06 +0900

    loops/main.go v0.1 time & strings package

--- 7/loops/main.go
+++ 7/loops/main.go
@@ -1,15 +1,33 @@
+// package main
+
+// import (
+// 	"fmt"
+// 	"reflect"
+// )
+
+// func main() {
+// 	var length float64 = 1.2
+// 	var width int = 2
+// 	fmt.Println("면적은", int(length)*width)
+// 	fmt.Println("길이 > 너비?", int(length) > width)
+// 	fmt.Println("형변환", reflect.TypeOf(int(length)))
+// 	fmt.Println("원본", reflect.TypeOf(int(length)))
+// }
+
 package main
 
 import (
 	"fmt"
-	"reflect"
+	"strings"
+	"time"
 )
 
 func main() {
-	var length float64 = 1.2
-	var width int = 2
-	fmt.Println("면적은", int(length)*width)
-	fmt.Println("길이 > 너비?", int(length) > width)
-	fmt.Println("형변환", reflect.TypeOf(int(length)))
-	fmt.Println("원본", reflect.TypeOf(int(length)))
+	var now time.Time = time.Now()
+	var day int = now.Day()
+	fmt.Println(day)
+	univ := "Go$ Inha$"
+	changer := strings.NewReplacer("$", "!")
+	changed := changer.Replace(univ)
+	fmt.Println(changed)
 }
