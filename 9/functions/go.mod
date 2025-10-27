28e6994ecf0cb43f6f27a821415534424bb74c79
gima2776 <gima2776@gmail.com>
2025-10-27 11:25:01 +0900

    functions/main.go v0.1

---
+++ 9/functions/go.mod
@@ -0,0 +1,3 @@
+module 9/functions
+
+go 1.25.1
---
+++ 9/functions/main.go
@@ -0,0 +1,19 @@
+package functions
+
+import "fmt"
+
+func swap(first int, second int) {
+	temp := 0
+	temp = first
+	first = second
+	second = temp
+	fmt.PrintLn(first, second)
+}
+
+func main() {
+	var a, b int = 10, 20
+	fmt.Println(a, b)
+	swap(a, b)
+	fmt.Println(a, b)
+	//fmt.Printf("%.2f\n", math.Sqrt(-25.0))
+}
